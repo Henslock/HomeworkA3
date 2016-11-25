63bdde45ec6292ad99f0b0af32be4fdbe8ef3abf
Henslock <henslock123@gmail.com>
2016-11-24 22:44:55 -0500

    Final Commit, Navmesh builder.

---
+++ Assets/Editor/LoadLevel_Editor.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LoadLevel_Editor : EditorWindow
+{
+    string levelFileName = "File Name...";
+    int xOffset, yOffset = 0;
+    TextAsset navLevel;
+
+    [MenuItem("Window/Level Options/Generate Navmesh Level")]
+    public static void ShowWindow()
+    {
+        LoadLevel_Editor window = GetWindow<LoadLevel_Editor>();
+        window.titleContent.text = "Generate Level Options";
+    }
+
+    void OnGUI()
+    {
+
+        levelFileName = GUILayout.TextField(levelFileName);
+
+        if (GUILayout.Button("Spawn Navigation Prefabs"))
+        {
+            navLevel = Resources.Load(levelFileName) as TextAsset;
+
+            GameObject navParent = new GameObject("Navmesh Blocks");
+
+            string[] lines = navLevel.text.Split("\n"[0]);
+
+            for (var i = 0; i < lines.Length; ++i)
+            {
+                string[] parts = lines[i].Split(","[0]);
+                for (var x = 0; x < parts.Length; ++x)
+                {
+                    if (parts[x] == "0" || parts[x] == "2" || parts[x] == "5")
+                    {
+                        Object navBlock = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/BasicBlock.prefab", typeof(GameObject));
+                        GameObject go = Instantiate(navBlock) as GameObject;
+                        go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = navParent.transform;
+                    }
+                    xOffset++;
+                }
+                xOffset = 0;
+                yOffset--;
+
+                Debug.Log("Finish Building Navigation Blocks");
+            }
+        }
+    }
+}
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,18 +5,22 @@ using System.IO;
 
 public class LevelLoader : MonoBehaviour {
 
-    public TextAsset Levelcsv;
+    public string FileName = "LevelFile";
+    private TextAsset Levelcsv;
     public GameObject[] blocks;
-    private GameObject go;
+    private GameObject go, levelBlocks;
     private int xOffset, yOffset = 0;
 
 	void Awake()
     {
-        ReadLevel();
+        Levelcsv = Resources.Load("LevelFile") as TextAsset;
+        levelBlocks = new GameObject("LevelBlocks");
+        ReadLevel(FileName);
     }
 
-    void ReadLevel()
+    public void ReadLevel(string file)
     {
+        Levelcsv = Resources.Load(file) as TextAsset;
         string[] lines = Levelcsv.text.Split("\n"[0]);
 
         for (var i = 0; i < lines.Length; ++i)
@@ -24,52 +28,61 @@ public class LevelLoader : MonoBehaviour {
             string[] parts = lines[i].Split(","[0]);
             for (var x = 0; x < parts.Length; ++x)
             {
-
+                //Spaghetti
                 switch (parts[x])
                 {
                     case "0":
                         //Basic Block
                         go = Instantiate(blocks[0]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "1":
                         //Spawn Point
                         blocks[1].transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "2":
                         //Super Bounce Block
                         go = Instantiate(blocks[2]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "3":
                         //Checkpoint
                         go = Instantiate(blocks[3]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "4":
                         //Death Block
                         go = Instantiate(blocks[4]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "5":
                         //Bounce Block
                         go = Instantiate(blocks[5]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "6":
                         //Light Source
                         go = Instantiate(blocks[6]);
                         go.transform.position = new Vector3(xOffset, yOffset, -1);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "7":
                         //Enemy
                         go = Instantiate(blocks[7]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                     case "8":
                         //End Goal
                         go = Instantiate(blocks[8]);
                         go.transform.position = new Vector3(xOffset, yOffset, 0);
+                        go.transform.parent = levelBlocks.transform;
                         break;
                 }
                 xOffset++;
